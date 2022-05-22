48cebdc69adbbcd4b14a948815d46661f87fa39c
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2022-05-22 17:21:02 +0300

    Add files via upload

---
+++ CREATE TABLE members.sql
@@ -0,0 +1,21 @@
+CREATE TABLE members
+(
+  member_id INT NOT NULL,
+  date_ex INT NOT NULL,
+  serial_number INT NOT NULL,
+  name INT NOT NULL,
+  price INT NOT NULL,
+  PRIMARY KEY (member_id)
+);
+
+CREATE TABLE card
+(
+  card_num INT NOT NULL,
+  Target_Audience INT NOT NULL,
+  Purchasing_group INT NOT NULL,
+  Route_type INT NOT NULL,
+  member_id INT NOT NULL,
+  PRIMARY KEY (card_num),
+  FOREIGN KEY (member_id) REFERENCES members(member_id)
+);
+

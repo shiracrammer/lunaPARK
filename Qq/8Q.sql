fefb90bc0e23797507bda255cc023435b02857d8
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2022-06-18 22:04:16 +0300

    Update 8Q.sql

--- Qq/8Q.sql
+++ Qq/8Q.sql
@@ -1,1 +1,2 @@
 /*8*/SELECT distinct e1.ID, e1.name,e1.salary, e2.ID, e2.name, e2.salary FROM employee e1, employee e2 WHERE e1.role='manager' and e2.role = 'manager' and e1.ID <> e2.ID and e1.salary= e2.salary;
+

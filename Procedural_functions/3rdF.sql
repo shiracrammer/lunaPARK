5194674485a9981763b95fdc193b98ffb5963614
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2022-06-18 12:54:43 +0300

    Add files via upload

---
+++ Procedural_functions/3rdF.sql
@@ -0,0 +1,11 @@
+create or replace function GetAmountKids return number amount
+is
+FUNCres number amount:=0;
+begin
+  select route_type into route_type;
+  from cardmember
+  where route_type='kid';
+  amount:=cont (cardmember.route_type);
+  return(FUNCres);
+  end GetAmountKids;
+  
---
+++ Procedural_functions/pro2.sql
@@ -0,0 +1,15 @@
+create or replace procedure isKID  (memberID in number,ans out varchar2)
+is
+checker number;
+ans varchar2 (5) NOT NULL:="False";
+begin
+select MEM  into checker
+from members
+where memberID=MEM and cardmember.route_type ='kid';
+if (SQL%found)
+then
+  ans:="True":
+else
+  ans;="False":
+end if;
+end isKID;

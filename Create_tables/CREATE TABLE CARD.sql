6234b67e6142b4f61a9983ae399a700264733ee3
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2022-06-18 12:48:44 +0300

    Add files via upload

---
+++ Create_tables/CREATE TABLE CARD.sql
@@ -0,0 +1,21 @@
+CREATE TABLE CARD
+(
+  cardID INT NOT NULL,
+  PRIMARY KEY (cardID)
+);
+
+CREATE TABLE employee
+(
+  ID INT NOT NULL,
+  name VARCHAR(20) NOT NULL,
+  role VARCHAR(20) NOT NULL,
+  address VARCHAR(35) NOT NULL,
+  salary FLOAT NOT NULL,
+  startDATE DATE NOT NULL,
+  cardID INT NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (cardID) REFERENCES CARD(cardID)
+);
+
+drop table card
+drop table employee

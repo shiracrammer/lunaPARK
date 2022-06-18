ddda33de6ee1da284dddb0732301f1189ba1a78d
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2022-06-18 22:01:42 +0300

    Update 10Q.sql

--- Qq/10Q.sql
+++ Qq/10Q.sql
@@ -1,2 +1,3 @@
 /*10*/SELECT card_num, member_id, name, date_ex FROM cardMEMBER NATURAL JOIN MEMBERS WHERE Route_type = 'adult' MINUS 
 SELECT card_num, member_id, name, date_ex FROM cardMEMBER NATURAL JOIN MEMBERS WHERE date_ex >= '23-june-2022' and Route_type = 'adult';
+

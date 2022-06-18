171674842a25b7fcd91fbd46736a2de5576a85e0
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2022-06-18 22:03:43 +0300

    Update 6Q.sql

--- Qq/6Q.sql
+++ Qq/6Q.sql
@@ -1,3 +1,4 @@
 /*6*/ SELECT cM1.card_num, cM2.Card_Num, cM1.member_id, cM2.Member_Id FROM cardMEMBER cM1, cardMEMBER cM2 WHERE cM1.member_id= cM2.member_id
              and  cM1.Purchasing_group = cM2.Purchasing_Group
             and cM1.Route_type <> cM2.Route_type;
+

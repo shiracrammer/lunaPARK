f66e96978a66c8186ff0f26007fe966d1a468eb2
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2022-06-18 22:05:04 +0300

    Update integration12.06.sql

--- integration12.06.sql
+++ integration12.06.sql
@@ -2,3 +2,4 @@
 /*2*/select distinct visitor_phone, visitor_name, Route_type as visitor_type, visitor_ID as member_id FROM rofridma.visitor NATURAL JOIN cardMEMBER WHERE Route_type = 'adult';
 /*3*/ select distinct name, employee_id as ID, facilities_location, role FROM rofridma.facilities  NATURAL JOIN employee WHERE facilities_location = 'north' and role = 'facility operator';
 /*4*/ select distinct name as name_mem, member_id, event_price as price, event_id, Purchasing_group FROM rofridma.event NATURAL JOIN members NATURAL JOIN cardMEMBER WHERE event_id = 1 or event_id = 5;
+

65931e96b9216c2dc3e15c2b13e89a28f30ec623
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2022-06-18 12:55:00 +0300

    Delete 2ndF.sql

--- 2ndF.sql
+++
@@ -1,8 +0,0 @@
- create or replace function find_employee (employee_id_to_find in number) return employeeNAME out varchar2 is
- FunctionResult employeeNAME out varchar2;
- begin
-   select employeeNAME into employeeNAME;
-   from employee
-   where employee_id_to_find:=employee.id;
-   return (employeeNAME);
-  end find_name;
